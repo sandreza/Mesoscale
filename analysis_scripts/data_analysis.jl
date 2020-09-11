87940925bf90114e1d79e1d17f6ef4fb6f68d6a8
sandreza <andrenogueirasouza@gmail.com>
2020-09-10 21:10:05 -0400

    properly labels equations

--- analysis_scripts/data_analysis.jl
+++ analysis_scripts/data_analysis.jl
@@ -191,29 +191,29 @@ u_avg = (avg_other(u,1)[1:end-1,:,:] + avg_other(u,1)[2:end,:,:]) ./ 2
 v_avg = (avg_other(v,1)[1:end-1,:,:] + avg_other(v,1)[2:end,:,:]) ./ 2
 w_avg = (avg_other(w,1)[1:end-1,:,:] + avg_other(w,1)[2:end,:,:]) ./ 2
 U = [u_avg, v_avg, w_avg]
-coriolis_force = [Ω[3] .* v_avg, - Ω[3] .* u_avg, zero_ϕ]
+coriolis_force = [-Ω[3] .* v_avg,  Ω[3] .* u_avg, zero_ϕ]
 
 norm(coriolis_force[1] .+ ∇pʰ[1]) / norm(coriolis_force[1])
 norm(coriolis_force[2] .+ ∇pʰ[2]) / norm(coriolis_force[2])
 
 ##
-field_label = [(coriolis_force[1], "Ω v"), (-∇pʰ[1], "-∂x(pʰ)"), (coriolis_force[2], "-Ω u"), (-∇pʰ[2], "-∂y(pʰ)")]
-selection = 2
+field_label = [(coriolis_force[1], "-Ω v"), (-∇pʰ[1], "-∂x(pʰ)"), (coriolis_force[2], "Ω u"), (-∇pʰ[2], "-∂y(pʰ)")]
+selection = 2+2
 field = field_label[selection][1]
 label = field_label[selection][2]
 cmax = maximum(field)
 cmin = minimum(field)
 clims = (cmin, cmax)
 p1 = contourf(x2, y2, field[ :, :, end]', 
-    color = :thermometer, title = "Surface " * label,
+    color = :thermometer, title = "100 [m] depth " * label,
     xlabel = "Zonal [m]", ylabel = "Meridional [m]"
     , clims = clims, linewidth = 0)
 
-selection = 1
+selection = 1+2
 field = field_label[selection][1]
 label = field_label[selection][2]
 p2 = contourf(x2, y2, field[ :, :, end]', 
-    color = :thermometer, title = "Surface " * label,
+    color = :thermometer, title = "100 [m] depth " * label,
     xlabel = "Zonal [m]", yaxis = false
     , clims = clims, linewidth = 0)
 plot(p1,p2)
