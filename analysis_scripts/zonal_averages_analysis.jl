1569a5bf48ac598d7529d0dccf8ea259cbda7ba0
sandreza <andrenogueirasouza@gmail.com>
2020-10-06 10:01:28 -0400

    small changes

--- analysis_scripts/zonal_averages_analysis.jl
+++ analysis_scripts/zonal_averages_analysis.jl
@@ -3,7 +3,7 @@ using LaTeXStrings
 include(pwd() * "/analysis_scripts/" * "post_analysis.jl")
 
 
-filename = "/Weno_20_zonal_averages.jld2"
+filename = "/L_Weno_20_zonal_averages.jld2"
 file = jldopen( pwd() * filename)
 z = file["grid"]["zC"][2:end-1]
 y = file["grid"]["yC"][2:end-1]
@@ -30,14 +30,15 @@ function plot_field(a; name = " ")
     display(p1)
 end
 ##
+tmp = norm(data_dictionary["u"][end] - data_dictionary["u"][end-1])
+tmp /= norm(data_dictionary["u"][end])
 
-
-vb = mean(data_dictionary["vb"][2:end])
+vb = mean(data_dictionary["vb"][end-20:end])
 vb = (vb[2:end, :] + vb[1:end-1, :]) ./ 2
-b = mean(data_dictionary["b"][2:end])
+b = mean(data_dictionary["b"][end-20:end])
 # plot_field(b, name = "b")
-u = mean(data_dictionary["u"][2:end])
-v = mean(data_dictionary["v"][2:end])
+u = mean(data_dictionary["u"][end-20:end])
+v = mean(data_dictionary["v"][end-20:end])
 v = (v[2:end, :] + v[1:end-1, :]) ./ 2
 vpbp = vb - v .* b
 f  = -1e-4
@@ -66,8 +67,8 @@ p1 = contourf(py, pz, a',
 
 
 ##
-b = mean(data_dictionary["v"][2:end])
-plot_field(b, name = "v")
+b = mean(data_dictionary["b"][2:end])
+plot_field(b, name = "b")
 
 ##
 
