8e777cac05bead0c51a12c855389225a731616e5
sandreza <andrenogueirasouza@gmail.com>
2020-10-29 14:39:05 -0400

    script modifications

--- analysis_scripts/nc_makie.jl
+++ analysis_scripts/nc_makie.jl
@@ -1,20 +1,18 @@
 using NetCDF, Plots, GLMakie, AbstractPlotting
 using ImageTransformations, Colors
+using AbstractPlotting.MakieLayout
 ##
 filename = pwd() * "/New_Weno_20_b.nc"
 ncinfo(filename)
-x = Array(NetCDF.open(filename, "xC"))
-y = Array(NetCDF.open(filename, "yC"))
-z = Array(NetCDF.open(filename, "zC"))
+xC = Array(NetCDF.open(filename, "xC"))
+yC = Array(NetCDF.open(filename, "yC"))
+zC = Array(NetCDF.open(filename, "zC"))
 t = Array(NetCDF.open(filename, "time"))
 sim_day = t ./ 86400
-z = (z[2:end] + z[1:end-1]) / 2
 b = NetCDF.open(filename, "b")
 ##
-T = Array(b[:,:,:,1])
-T2 = Array(b[:,:,:,end])
-obs = Node(1)
-Ti = @lift Array(b[:,:,:,$obs])
+
+T = b[:,:,:,1]
 # AbstractPlotting.available_gradients() # see available colors
 cmap = :linear_blue_95_50_c20_n256
 cmap = :thermometer
@@ -23,26 +21,41 @@ cmapa = RGBAf0.(to_colormap(cmap), 1);
 cmapa1 = vcat(fill(RGBAf0(0,0,0,0), 10), cmapa);
 cmapa2 = vcat(cmapa, fill(RGBAf0(0,0,0,0), 1));
 x, y, z = size(T)
-scene = volume(0..x, 0..y, 0..z, T, colorrange=clims, algorithm=:absorption, absorption=5.0f0, colormap=cmapa2, show_axis=false)
+scene = volume(0..x, 0..y, 0..z, T, colorrange=clims, algorithm=:absorption, absorption=10.0f0, colormap=cmapa2, show_axis=false)
 
 ##
-α(ξ) = ξ  # Opacity/alpha along the cmap (0 <= ξ <= 1)
+zind = 20
+obs = Node(1) # make index a "node" to allow for movie making
+Ti = @lift Array(b[:,:,zind:end,$obs])
+α(ξ) = ξ^(1.2)  # Opacity/alpha along the cmap (0 <= ξ <= 1)
 cmap_rgb = to_colormap(:thermometer)
 A = α.(range(0, 1, length=length(cmap_rgb)))
+clims = ( minimum(T[:,:,zind:end]) * 1.0, maximum(T[:,:,zind:end]))
 cmap_rgba = RGBAf0.(cmap_rgb, A)
-scene = volume(0..x, 0..y, 0..z, Ti, colorrange=clims, colormap=cmap_rgba, algorithm=:absorption, absorption=10.0f0,
-               backgroundcolor=:black, show_axis=false)
 
+scene, layout = layoutscene(resolution = (1920, 1080),  backgroundcolor=:black)
+lscene = layout[1, 1] = LScene(scene)   
+volumeobj = volume!(lscene, 0..x, 0..y, 0..z, Ti, colorrange=clims, colormap=cmap_rgba,
+                     algorithm=:absorption, absorption=10.0f0
+        )
+cbar = layout[1, 2] = LColorbar(scene,
+     volumeobj, width = 50, height = 1000, absorption=10.0f0, ticklabelcolor = :white, ticklabelsize = 50)           
+# LColorbar(scene, colormap=cmap_rgba,  label = "Activity [spikes/sec]")
+scene
 ##
-zoom!(scene, (100, 00, 00), -1.25, false)
-θ = 0.005 * 2π
-rotate_cam!(scene, (θ, 0, 0))
+lscene
+##
+# LText(scene, "A", textsize = 35, font = "Noto Sans Bold", halign = :right)
+##
+zoom!(scene.children[1], (100, 00, 00), -1.25, false)
+θ = -0.005 * 2π
+rotate_cam!(scene.children[1], (θ, 0, 0))
 
 ##
 record(scene, "oceananigans_makie.gif", 1:length(t), framerate=10) do n
     obs[] = n
-    n == 1 && zoom!(scene, (0, 0, 0), -1.25, false)
-    rotate_cam!(scene, (θ, 0, 0))
+    n == 1 && zoom!(scene.children[1], (0, 0, 0), -1.25, false)
+    rotate_cam!(scene.children[1], (θ, 0, 0))
 end
 #=
 
