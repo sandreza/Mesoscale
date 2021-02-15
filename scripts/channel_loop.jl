12f27923df3d33fccfffa718cb42e70ff645920b
sandreza <andrenogueirasouza@gmail.com>
2021-02-15 16:54:43 -0500

    test list

--- scripts/channel_loop.jl
+++ scripts/channel_loop.jl
@@ -2,9 +2,13 @@
 include(pwd() * "/scripts/dependencies.jl")
 include(pwd() * "/scripts/loop_names.jl")
 
-
 Qlist = [10, 1, 5]
 τlist = [0.2, 0.01, 0.05, 0.1]
+
+## Test first
+Qlist = [10]
+τlist = [0.2]
+
 
 for Q in Qlist, τ in τlist
     simulationdescription = generate_descriptor(Q, τ)
